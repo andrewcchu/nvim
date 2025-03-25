0fbe5bc4dc9b795ba1d783a275e8374c96ed1dba
Andrew Chu <andy.chunan.chu@gmail.com>
2025-03-25 13:57:54 -0500

    Ignore default colorschemes

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,3 +1,30 @@
+vim.opt.wildignore:append({
+	  "blue.vim",
+	  "darkblue.vim",
+	  "delek.vim",
+	  "desert.vim",
+	  "elflord.vim",
+	  "evening.vim",
+	  "industry.vim",
+	  "habamax.vim",
+	  "koehler.vim",
+	  "lunaperche.vim",
+	  "morning.vim",
+	  "murphy.vim",
+	  "pablo.vim",
+	  "peachpuff.vim",
+	  "quiet.vim",
+	  "ron.vim",
+	  "shine.vim",
+	  "slate.vim",
+	  "sorbet.vim",
+	  "retrobox.vim",
+	  "torte.vim",
+	  "wildcharm.vim",
+	  "zaibatsu.vim",
+	  "zellner.vim",
+})
+
 vim.opt.background = "dark" -- set this to dark or light
 vim.opt.termguicolors = true
 vim.cmd.colorscheme "no-clown-fiesta"
