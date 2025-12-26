acf99b94203606a6403d6f2c552f0454caa499c3
Andrew Chu <andy.chunan.chu@gmail.com>
2025-12-25 21:38:50 -0700

    Update for nvim-treesitter switch to main

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,7 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = { "lua", "luadoc", "python", "vim", "vimdoc", "markdown", "markdown_inline", "latex", "rust" },
